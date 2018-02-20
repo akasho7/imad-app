21d02a9c5353cbd3489b44bb04b5d2af1e10e993
akasho7 <ahakash1005@gmail.com>
2018-02-20 20:02:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+app.get('/cse1',function(req,res){
+    res.send('please avoid me iam dangerous to you');
+});
 
 
 // Do not change port, otherwise your app won't run on IMAD servers
