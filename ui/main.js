6f6d6e9d6cb1318979b1948a4319cd1b5b1090fa
akasho7 <ahakash1005@gmail.com>
2018-02-21 20:01:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,10 @@ console.log('Loaded!');
 // change element from the console
  var element=document.getElementById('main_text');
  element.innerHTML = 'iam welcome you';
+ 
+ // 
+ var img=document.getElementById('mady');
+ mady.onclick=function();
+ {
+     img.style.marginLeft='100px';
+ }
